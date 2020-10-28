0e40ccb6ca42edb487f3fe603862a9c86653f58c
David Kovacs <davidkovacs94@yahoo.com>
2020-10-28 21:17:26 +0100

    Visualization added

--- epstein_civil_violence/server.py
+++ epstein_civil_violence/server.py
@@ -6,7 +6,10 @@ from .agent import Citizen, Cop
 
 
 COP_COLOR = "#000000"
-AGENT_QUIET_COLOR = "#0066CC"
+AGENT_QUIET_EMPLOYED_NOT_CORRUPT_COLOR = "#0066CC"
+AGENT_QUIET_EMPLOYED_CORRUPT_COLOR = "#CC6600"
+AGENT_QUIET_UNEMPLOYED_NOT_CORRUPT_COLOR = "#00CC14"
+AGENT_QUIET_UNEMPLOYED_CORRUPT_COLOR = "#F2FF00" 
 AGENT_REBEL_COLOR = "#CC0000"
 JAIL_COLOR = "#757575"
 
@@ -23,9 +26,14 @@ def citizen_cop_portrayal(agent):
     }
 
     if type(agent) is Citizen:
-        color = (
-            AGENT_QUIET_COLOR if agent.condition == "Quiescent" else AGENT_REBEL_COLOR
-        )
+        if (agent.is_employed == 1 and agent.moral_state != "Corrupted"):
+            color = (AGENT_QUIET_EMPLOYED_NOT_CORRUPT_COLOR if agent.condition == "Quiescent" else AGENT_REBEL_COLOR)
+        elif (agent.is_employed == 1 and agent.moral_state == "Corrupted"):
+            color = (AGENT_QUIET_EMPLOYED_CORRUPT_COLOR if agent.condition == "Quiescent" else AGENT_REBEL_COLOR)
+        elif (agent.is_employed == 0 and agent.moral_state != "Corrupted"):
+            color = (AGENT_QUIET_UNEMPLOYED_NOT_CORRUPT_COLOR if agent.condition == "Quiescent" else AGENT_REBEL_COLOR)
+        elif (agent.is_employed == 0 and agent.moral_state == "Corrupted"):
+            color = (AGENT_QUIET_UNEMPLOYED_CORRUPT_COLOR if agent.condition == "Quiescent" else AGENT_REBEL_COLOR)    
         color = JAIL_COLOR if agent.jail_sentence else color
         portrayal["Color"] = color
         portrayal["r"] = 0.8
@@ -47,14 +55,9 @@ model_params = dict(
     cop_vision=7,
     legitimacy=0.8,
     max_jail_term=1000,
-    initialUnEmployment = 0.1
 )
-  
-def main():
-    canvas_element = CanvasGrid(citizen_cop_portrayal, 40, 40, 480, 480)
+
+canvas_element = CanvasGrid(citizen_cop_portrayal, 40, 40, 480, 480)
 server = ModularServer(
     EpsteinCivilViolence, [canvas_element], "Epstein Civil Violence", model_params
 )
-if __name__ == "__main__":
-    main()
-
